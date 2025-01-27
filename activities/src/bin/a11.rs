95eda88f8977f70f3a2fe59b010d55a257aab157
Courtney McCurry <parkersoncourtney@gmail.com>
2025-01-27 00:26:35 -0600

    first commit to new repo

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -9,5 +9,25 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct GroceryItem {
+    quantity: i32,
+    id_number: i32,
+}
+
+fn display_quantity(item: &GroceryItem) {
+    println!("quantity = {:?}", item.quantity);
+}
+
+fn display_id_number(item: &GroceryItem) {
+    println!("id_number = {:?}", item.id_number);
+}
+
+fn main() {
+    let pasta = GroceryItem {
+        quantity: 14,
+        id_number: 4000, 
+    };
+    display_quantity(&pasta);
+    display_id_number(&pasta);
+}
 
--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,5 +11,39 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
 
+struct Person {
+    age: i32,
+    name: String,
+    color: String, 
+}
+
+fn print(data: &str) {
+    println!("{:?}", data); 
+}
+
+fn main() {
+    let people = vec! [
+        Person {
+            age: 8,
+            name: "Courtney".to_owned(),
+            color: "blue".to_owned(),
+        },
+        Person {
+            age: 39,
+            name: "Jon-Erik".to_owned(),
+            color: "green".to_owned(),
+        },
+        Person {
+            age: 10,
+            name: "KK".to_owned(),
+            color: "blue".to_owned(),
+        }
+    ]; 
+    for person in people {
+        if person.age <= 10 {
+            print(&person.name);
+            print(&person.color);
+        }
+    }
+}
